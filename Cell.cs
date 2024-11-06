2a4b963e4c160bfbebaa3dfcce250e8169d2a114
Troupote <73849862+Troupote@users.noreply.github.com>
2024-11-06 14:31:49 +0100

    ajout des attributs et des méthodes de cell.cs

--- Cell.cs
+++ Cell.cs
@@ -8,5 +8,42 @@ namespace JeuDeLaVieConsole
 {
     public class Cell
     {
+
+        private bool _isAlive;
+        public bool isAlive
+        {
+            get { return _isAlive; }
+            set { _isAlive = value; }
+        }
+
+        private bool _nextState;
+
+        public bool nextState
+        {
+            get { return _nextState; }
+            set { _nextState = value; }
+
+        }
+
+        public Cell(bool state)
+        {
+            isAlive = state;
+        }
+
+        public void ComeAlive()
+        {
+            nextState = true;
+        }
+
+        public void PassAway()
+        {
+            nextState = false;
+        }
+
+        public void Update()
+        {
+            isAlive = nextState;
+        
+        }
     }
 }
